f0809c5d036c335c4f5678f707d7ebc63fabd3d5
sammanamgain <lionalmessi609@gmail.com>
2023-12-04 21:57:45 +0545

    deletereview and getallrreviews

--- backend/Models/productModel.js
+++ backend/Models/productModel.js
@@ -1,79 +1,75 @@
 const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
-    name: {type: String, required: [true, "Please Enter the product Name"]},
-    description: {
-        type: String,
-        required: [true, "please Enter Product Description"],
-    },
-    price: {
-        type: Number,
-        required: [true, "pleae Enter the proudce description"],
-    },
+  name: { type: String, required: [true, "Please Enter the product Name"] },
+  description: {
+    type: String,
+    required: [true, "please Enter Product Description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "pleae Enter the proudce description"],
+  },
 
-    user: {
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, "Please Enter Product Category"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please Enter Product Stock"],
+    maxlength: [4, "Stock cann't exceed 4 character"],
+    default: 1,
+  },
+  numofReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      user: {
         type: mongoose.Schema.ObjectId,
         ref: "User",
         required: true,
-    },
-    ratings: {
-        type: Number,
-        default: 0,
-    },
-    images: [
-        {
-            public_id: {
-                type: String,
-                required: true,
-            },
-            url: {
-                type: String,
-                required: true,
-            },
-        },
-    ],
-    category: {
+      },
+      name: {
         type: String,
-        required: [true, 'Please Enter Product Category']
-
-    },
-    stock: {
+        required: true,
+      },
+      rating: {
         type: Number,
-        required: [true, 'Please Enter Product Stock'],
-        maxlength: [4, "Stock cann't exceed 4 character"],
-        default: 1
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
     },
-    numofReviews: {
-        type: Number,
-        default: 0
-
-    },
-    reviews: [
-
-        {
-            user: {
-                type: mongoose.Schema.ObjectId,
-                ref: "User",
-                required: true,
-            },
-            name: {
-                type: String,
-                required: true
-            },
-            rating: {
-                type: Number,
-                required: true
-            },
-            comment: {
-                type: String,
-                required: true
-            }
-        }
-    ],
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
-
-module.exports = mongoose.model("product", productSchema)
+module.exports = mongoose.model("product", productSchema);
--- backend/routes/productroutes.js
+++ backend/routes/productroutes.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const {
-    getallproducts,
-    createproduct,
-    updateproduct,
-    deleteproduct,
-    getSingleProduct, createproductreview,
+  getallproducts,
+  createproduct,
+  updateproduct,
+  deleteproduct,
+  getSingleProduct,
+  createproductreview,
+  getproductReviews,
+  deleteReview,
 } = require("../controller/productcontroller");
-const {isAuthenticatedUser, authorizedroles} = require("../middleware/auth");
+const { isAuthenticatedUser, authorizedroles } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/product").get(getallproducts);
 router
-    .route("/admin/product/new")
-    .post(isAuthenticatedUser, authorizedroles("admin"), createproduct);
+  .route("/admin/product/new")
+  .post(isAuthenticatedUser, authorizedroles("admin"), createproduct);
 router
-    .route("/admin/product/:id")
-    .get(getSingleProduct)
-    .put(isAuthenticatedUser, authorizedroles("admin"), updateproduct)
-    .delete(isAuthenticatedUser, authorizedroles("admin"), deleteproduct);
-router.route("/product/:id").get(getSingleProduct)
-router.route("/review").put(isAuthenticatedUser, createproductreview)
+  .route("/admin/product/:id")
+  .get(getSingleProduct)
+  .put(isAuthenticatedUser, authorizedroles("admin"), updateproduct)
+  .delete(isAuthenticatedUser, authorizedroles("admin"), deleteproduct);
+router.route("/product/:id").get(getSingleProduct);
+router.route("/review").put(isAuthenticatedUser, createproductreview);
+router
+  .route("/reviews")
+  .get(getproductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
 module.exports = router;
