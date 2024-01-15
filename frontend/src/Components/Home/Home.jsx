7a570a486c9befe7347d4e34a050ae4a61c8d50a
sammanamgain <amgain02@gmail.com>
2024-01-15 16:44:34 +0545

    scrollbar updated

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,41 +1,50 @@
 import React from "react";
 import { CgMouse } from "react-icons/cg";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import "./Home.css";
-import Product  from "./Product";
+import Product from "./Product";
 export default function Home() {
   const product = {
     name: "shirt",
     price: "10000",
     _id: "samman",
-    images: [{ url: "https://i2.wp.com/www.marknepal.com/wp-content/uploads/2019/09/IMG-6bd6b5be52c5c7cdc20af3b0ff60099f-V.jpg?fit=1280%2C1280&ssl=1" }]
+    images: [
+      {
+        url: "https://i2.wp.com/www.marknepal.com/wp-content/uploads/2019/09/IMG-6bd6b5be52c5c7cdc20af3b0ff60099f-V.jpg?fit=1280%2C1280&ssl=1",
+      },
+    ],
   };
   return (
     <>
       <div className='banner'>
         <p className='text-2xl'> Welcome to Ecommerce</p>
         <h1> FIND AMAZING PROUDCTS BELOW</h1>
-        <Link
-          to=''
+        <a
+          href='#Featured'
           className='flex text-black custom border-2 rounded-2xl p-2 border-black bg-white hover:bg-inherit'
         >
           <button className=''> Scroll</button>
           <CgMouse className='mt-1' />
-        </Link>
+        </a>
       </div>
-      <div className='flex justify-center items-center py-2  w-30 border-b-lime-600 '>
-        <h1 className=' font-mono text-2xl border-b-4'> Featured Products</h1>
-      </div>
-      <div className='flex md:flex-row flex-col items-center justify-center md:items-start md:justify-start flex-wrap md:ml-20 '>
-        <Product product={product} />
-        <Product product={product} />
-        <Product product={product} />
-        <Product product={product} />
-        <Product product={product} />
-        <Product product={product} />
-        <Product product={product} />
-        <Product product={product} />
-        <Product product={product} />
+      <div id='Featured' className='mt-10 pt-10'>
+        <div className='flex justify-center items-center py-2  w-30 border-b-lime-600 '>
+          <h1 className=' font-mono text-2xl border-b-4'> Featured Products</h1>
+        </div>
+        <div
+          className='flex md:flex-row flex-col items-center justify-center md:items-start md:justify-start flex-wrap md:ml-20 '
+          id='Homeproduct'
+        >
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+        </div>
       </div>
     </>
   );
