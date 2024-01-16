4fece2e4e4ea7c857ce3211a171ae1cfbb8c10f6
sammanamgain <amgain02@gmail.com>
2024-01-16 13:23:01 +0545

    cors origin request issue fixed

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,18 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const product = require('./routes/productroutes');
+app.use(cors());
+
+const product = require("./routes/productroutes");
 const user = require("./routes/userRoutes");
-const order=require("./routes/orderRoutes")
-const error = require('./middleware/customError');
-const cookie_parser=require('cookie-parser');
+const order = require("./routes/orderRoutes");
+const error = require("./middleware/customError");
+const cookie_parser = require("cookie-parser");
 app.use(express.json());
 app.use(cookie_parser());
-app.use('/api/v1', product);
-app.use('/api/v1', user)
-app.use('/api/v1',order)
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
 //calling error handler middleware
-app.use(error)
+app.use(error);
 module.exports = app;
